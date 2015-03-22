b497a55b655714922bfec6379404f331905037cd
olegsmetanin <olegsmith@gmail.com>
2015-03-22 11:28:41 +0300

    add Router context test

---
+++ test/test.es7.js
@@ -0,0 +1,2 @@
+import './tests/Promise.es7.js';
+import './tests/Router.es7.jsx';
---
+++ test/tests/Router.es7.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Router from 'react-router';
+import { Route, RouteHandler, DefaultRoute, State, Link, Redirect } from 'react-router';
+import should from 'should';
+
+describe('Router', function () {
+
+    it('router context available in componentWillMount', (done) => {
+        
+        class Page extends React.Component {
+            constructor (props) {
+                super(props);
+                should.not.exist(this.context);
+            }
+
+            componentWillMount() {
+                this.context.should.have.property('router');
+                this.context.router.getCurrentPath().should.equal('/home');
+            }
+
+            render() {
+                this.context.should.have.property('router');
+                this.context.router.getCurrentPath().should.equal('/home');
+                return <div></div>
+            }
+        }
+
+        Page.contextTypes = {
+          router: React.PropTypes.func.isRequired
+        };
+
+        let routes = <Route path="/home" handler={Page}/>
+
+        Router.run(routes, '/home', function (Handler, state) {
+            React.renderToString(<Handler />)
+            done();
+        });
+
+    })
+})
