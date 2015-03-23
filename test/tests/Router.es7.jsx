d2571749c0d5ea6222dfbe4031d9df7b307a2eb3
olegsmetanin <olegsmith@gmail.com>
2015-03-23 13:47:11 +0300

    Router test identation

--- test/tests/Router.es7.jsx
+++ test/tests/Router.es7.jsx
@@ -5,88 +5,89 @@ import should from 'should';
 
 describe('Router', function () {
 
-    it('router context is available in componentWillMount', (done) => {
+  it('router context is available in componentWillMount', (done) => {
 
-        class Widget extends React.Component {
-            constructor (props) {
-                super(props);
-                should.not.exist(this.context);
-            }
+    class Widget extends React.Component {
+      constructor (props) {
+        super(props);
+        should.not.exist(this.context);
+      }
 
-            componentWillMount() {
-                this.context.should.have.property('router');
-                this.context.router.getCurrentPath().should.equal('/page/1');
-                this.context.router.getCurrentParams().should.have.property('id', '1');
-            }
+      componentWillMount() {
+        this.context.should.have.property('router');
+        this.context.router.getCurrentPath().should.equal('/page/1');
+        this.context.router.getCurrentParams().should.have.property('id', '1');
+      }
 
-            render() {
-                this.context.should.have.property('router');
-                this.context.router.getCurrentPath().should.equal('/page/1');
-                this.context.router.getCurrentParams().should.have.property('id', '1');
-                return <div></div>;
-            }
-        }
+      render() {
+        this.context.should.have.property('router');
+        this.context.router.getCurrentPath().should.equal('/page/1');
+        this.context.router.getCurrentParams().should.have.property('id', '1');
+        return <div></div>;
+      }
+    }
 
-        Widget.contextTypes = {
-          router: React.PropTypes.func.isRequired,
-        };
+    Widget.contextTypes = {
+      router: React.PropTypes.func.isRequired,
+    };
 
-        class Page extends React.Component {
-            constructor (props) {
-                super(props);
-                should.not.exist(this.context);
-            }
+    class Page extends React.Component {
+      constructor (props) {
+        super(props);
+        should.not.exist(this.context);
+      }
 
-            componentWillMount() {
-                this.context.should.have.property('router');
-                this.context.router.getCurrentPath().should.equal('/page/1');
-                this.context.router.getCurrentParams().should.have.property('id', '1');
-            }
+      componentWillMount() {
+        this.context.should.have.property('router');
+        this.context.router.getCurrentPath().should.equal('/page/1');
+        this.context.router.getCurrentParams().should.have.property('id', '1');
+      }
 
-            render() {
-                this.context.should.have.property('router');
-                this.context.router.getCurrentPath().should.equal('/page/1');
-                this.context.router.getCurrentParams().should.have.property('id', '1');
-                return <div><Widget/></div>;
-            }
-        }
+      render() {
+        this.context.should.have.property('router');
+        this.context.router.getCurrentPath().should.equal('/page/1');
+        this.context.router.getCurrentParams().should.have.property('id', '1');
+        return <div><Widget/></div>;
+      }
+    }
 
-        Page.contextTypes = {
-          router: React.PropTypes.func.isRequired,
-        };
+    Page.contextTypes = {
+      router: React.PropTypes.func.isRequired,
+    };
 
-        class Layout extends React.Component {
-            constructor (props) {
-                super(props);
-                should.not.exist(this.context);
-            }
+    class Layout extends React.Component {
+      constructor (props) {
+        super(props);
+        should.not.exist(this.context);
+      }
 
-            componentWillMount() {
-                this.context.should.have.property('router');
-                this.context.router.getCurrentPath().should.equal('/page/1');
-                this.context.router.getCurrentParams().should.have.property('id', '1');
-            }
+      componentWillMount() {
+        this.context.should.have.property('router');
+        this.context.router.getCurrentPath().should.equal('/page/1');
+        this.context.router.getCurrentParams().should.have.property('id', '1');
+      }
 
-            render() {
-                this.context.should.have.property('router');
-                this.context.router.getCurrentPath().should.equal('/page/1');
-                this.context.router.getCurrentParams().should.have.property('id', '1');
-                return <div><RouteHandler/></div>;
-            }
-        }
+      render() {
+        this.context.should.have.property('router');
+        this.context.router.getCurrentPath().should.equal('/page/1');
+        this.context.router.getCurrentParams().should.have.property('id', '1');
+        return <div><RouteHandler/></div>;
+      }
+    }
 
-        Layout.contextTypes = {
-          router: React.PropTypes.func.isRequired,
-        };
+    Layout.contextTypes = {
+      router: React.PropTypes.func.isRequired,
+    };
 
-        let routes = <Route handler={Layout}>
-                <Route path="/page/?:id?" handler={Page}/>
-            </Route>;
+    let routes = <Route handler={Layout}>
+      <Route path="/page/?:id?" handler={Page}/>
+    </Route>;
 
-        Router.run(routes, '/page/1', function (Handler) {
-            React.renderToString(<Handler />);
-            done();
-        });
-
+    Router.run(routes, '/page/1', function (Handler) {
+      React.renderToString(<Handler />);
+      done();
     });
+
+  });
+
 });
